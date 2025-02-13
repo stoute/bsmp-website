6c99c053b3c9096cb0b16f7b82ad9ea754bafbb4
Bob Stoute <stoute.bob@gmail.com>
2025-02-13 18:21:27 +0100

    feat: enabled ssr on firebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,24 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-
+import node from "@astrojs/node";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-sphere-demo.vercel.app",
-  // base: '/',
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({
-    applyBaseStyles: false
-  }), icon()]
+  output: "server", // Use 'server' or 'hybrid' for SSR
+  base: "/",
+  adapter: node({
+    mode: "middleware",
+  }),
+  integrations: [
+    mdx(),
+    sitemap(),
+    solidJs(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+  ],
 });
